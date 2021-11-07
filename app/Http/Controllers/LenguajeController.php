2641897679998d39dd44b1891bd6692e53b1ad23
JoselinePavon <52256066+JoselinePavon@users.noreply.github.com>
2021-11-07 14:25:20 -0600

    Se agrego el navbar

--- app/Http/Controllers/LenguajeController.php
+++ app/Http/Controllers/LenguajeController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lenguaje;
 use Illuminate\Http\Request;
 
 class LenguajeController extends Controller
 {
-    //
+    //registro del lenguaje
+    public function registrar(){
+
+        return view('lenguaje.createLenguaje');
+    }
+
+    //Guardar Lenguaje
+    public function save(Request $request){
+
+        $validator = $this->validate($request, [
+            'descripcion_leng'=> 'required|unique:App\Models\Lenguaje,descripcion_lenguaje|string|max:45',
+        ]);
+
+        Lenguaje::create([
+            'descripcion_lenguaje'=>$validator['descripcion_leng']
+        ]);
+
+        return redirect('/lenguaje/read')->with('guardar', 'ok');
+    }
+
+    //Read Listado de lenguajes
+    public function read(){
+        $language['lenguajes'] = Lenguaje::paginate(7);
+
+        return view('lenguaje.readLenguaje', $language);
+    }
+
+    //Guardar lenguajes
+    public function updateForm($id){
+        $lenguajes = Lenguaje::findOrFail($id);
+
+        return view('lenguaje.updateLenguaje', compact( 'lenguajes'));
+    }
+
+    //Edicion de lenguajes
+    public function edit(Request $request, $id){
+        $dataLeng = request()->except((['_token','_method']));
+        Lenguaje::where('id', '=', $id)->update($dataLeng);
+
+        return redirect('/lenguaje/read')->with('editar', 'ok');
+    }
+
+    //Delete lenguajes
+    public function delete($id){
+        Lenguaje::destroy($id);
+
+        return back()->with('eliminar', 'ok');
+    }
 }
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,10 +7,56 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel=" stylesheet " href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <title>Examen Final</title>
 </head>
 <body>
+<div class="sidebar">
+    <a  href="#"><i class="fa fa-home fa-fw" aria-hidden="true"></i> &nbsp;Home</a>
+    <a href="#"><i class="fa fa-book fa-fw" aria-hidden="true"></i>&nbsp; Library</a>
+    <a  href="#"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i>&nbsp; Registry</a>
+    <a  href="#"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Settings</a>
+</div>
+<style>
+    .sidebar {
+        height: 100%;
+        width: 250px;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        background-color:#E9967A;
+        overflow-x: hidden;
+        padding-top: 18px;
+    }
 
+    /* Style sidebar links */
+    .sidebar a {
+        padding: 6px 8px 6px 16px;
+        text-decoration: none;
+        font-size: 35px;
+        color: white;
+        display: block;
+    }
+
+    /* Style links on mouse-over */
+    .sidebar a:hover {
+        color: #CD5C5C;
+    }
+
+    /* Style the main content */
+    .main {
+        margin-left: 160px; /* Same as the width of the sidenav */
+        padding: 0px 10px;
+    }
+
+    /* Add media queries for small screens (when the height of the screen is less than 450px, add a smaller padding and font-size) */
+    @media screen and (max-height: 450px) {
+        .sidebar {padding-top: 15px;}
+        .sidebar a {font-size: 18px;}
+    }
+
+</style>
 
 <div class="container">
     @yield('contenedor')
